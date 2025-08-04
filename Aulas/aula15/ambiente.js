631fb6ba9a48ab596058dd05b6bb3931bfb36047
Carlos Daniel <carlosdaniel8aa@gmail.com>
2025-08-04 11:10:55 -0300

    Conclusao

---
+++ Aulas/aula15/ambiente.js
@@ -0,0 +1,15 @@
+let num = [5, 8, 2, 9, 3]
+
+num.sort() 
+console.log(num)
+console.log(`O vetor tem  ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+for (let pos = 0; pos<num.length; pos++) {
+    console.log(num[pos])
+}
+let pos= num.indexOf(7)
+if (pos == -1) {
+    console.log(`O valor não foi enconcontrado!`)
+} else {
+console.log(`O valor 8 está na posição ${pos}`)
+}
